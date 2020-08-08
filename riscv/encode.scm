b2598a3ff024501a14ea7d7ea080d694826dec6d
Sören Tempel <soeren+git@soeren-tempel.net>
2020-08-08 23:08:38 +0200

    Perform basic sanity check on encoder arguments

--- riscv/encode.scm
+++ riscv/encode.scm
@@ -1,15 +1,19 @@
 (import srfi-151)
 
 (define new-instruction bitwise-ior)
-(define (new-field startpos value)
-  (arithmetic-shift value startpos))
+(define (new-field start size value)
+  (if (> value (expt 2 (- size 1)))
+    (error "given value too large for field"))
+    (arithmetic-shift value start))
 
 (define (r-type opcode funct3 funct7
           rs1 rs2 rd)
+  ;; TODO: Only pass the size, not the position.
+  ;; The position can be infered from size of previous field.
   (new-instruction
-    (new-field 25 funct7)
-    (new-field 20 rs2)
-    (new-field 15 rs1)
-    (new-field 12 funct3)
-    (new-field 07 rd)
-    opcode))
+    (new-field 25 07 funct7)
+    (new-field 20 05 rs2)
+    (new-field 15 05 rs1)
+    (new-field 12 03 funct3)
+    (new-field 07 05 rd)
+    (new-field 00 07 opcode)))
--- tests/encode.scm
+++ tests/encode.scm
@@ -1,4 +1,18 @@
 (import riscv test)
+(import (chicken condition))
+
+(define exception-raised #t)
+(define-syntax test-exception
+  (syntax-rules ()
+    ((test-exception body ...)
+     (call-with-current-continuation
+       (lambda (con)
+         (with-exception-handler
+           (lambda (e) (con exception-raised))
+           (lambda ()
+             (begin
+              (begin body ...)
+              (con (not exception-raised))))))))))
 
 (test-group "encode r-type"
   (test "encode add instruction"
@@ -7,6 +21,7 @@
             12 10 11))
 
   (test "encode add instruction with invalid RD"
-    0
-    (r-type #b0110011 #b000 #b0000000
-            12 10 42)))
+    exception-raised
+    (test-exception
+      (r-type #b0110011 #b000 #b0000000
+              12 10 42))))
