b39aa208632ce862084c91300ef3ca17a0987a60
Sören Tempel <soeren+git@soeren-tempel.net>
2020-08-08 21:35:13 +0200

    Embrace »code is data« in the encoder

--- encode.scm
+++ encode.scm
@@ -1,11 +1,15 @@
 (import srfi-151)
 
+(define new-instruction bitwise-ior)
+(define (new-field startpos value)
+  (arithmetic-shift value startpos))
+
 (define (new-r-type opcode funct3 funct7
           rs1 rs2 rd)
-  (bitwise-ior
-    (arithmetic-shift funct7 25)
-    (arithmetic-shift rs2 20)
-    (arithmetic-shift rs1 15)
-    (arithmetic-shift funct3 12)
-    (arithmetic-shift rd 7)
+  (new-instruction
+    (new-field 25 funct7)
+    (new-field 20 rs2)
+    (new-field 15 rs1)
+    (new-field 12 funct3)
+    (new-field 07 rd)
     opcode))
