948b2a589bee0c48e137f059888d445153cc1c8e
Sören Tempel <soeren+git@soeren-tempel.net>
2020-08-08 14:21:51 +0200

    Add wrapper around bit-field function

--- decode.scm
+++ decode.scm
@@ -1,27 +1,34 @@
 (import srfi-151)
 
+;; The instr-field function from SRFI-151 returns a bit field in the
+;; interval [start, end - 1]. Contrary to instr-field, this functions
+;; returns a field in the interval [start, end] to allow using the
+;; same instr-field indices as speciified by the RISC-V specification.
+(define (instr-field instr start end)
+  (bit-field instr start (+ end 1)))
+
 (define (instr-opcode instr)
-  (bit-field instr 0 6))
+  (instr-field instr 0 6))
 
 (define (instr-funct3 instr)
-  (bit-field instr 12 14))
+  (instr-field instr 12 14))
 
 (define (instr-funct7 instr)
-  (bit-field instr 25 31))
+  (instr-field instr 25 31))
 
 (define (instr-rs1 instr)
-  (bit-field instr 15 19))
+  (instr-field instr 15 19))
 
 (define (instr-rs2 instr)
-  (bit-field instr 20 24))
+  (instr-field instr 20 24))
 
 (define (instr-rd instr)
-  (bit-field instr 7 11))
+  (instr-field instr 7 11))
 
 (define (instr-i-imm instr)
-  (bit-field instr 20 31))
+  (instr-field instr 20 31))
 
 (define (instr-s-imm instr)
   (bitwise-ior
-    (bit-field instr 25 31)
-    (bit-field instr 7 11)))
+    (instr-field instr 25 31)
+    (instr-field instr 7 11)))
