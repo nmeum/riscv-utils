216bba265244eae65ebaf7e40f3697f747fa771a
Sören Tempel <soeren+git@soeren-tempel.net>
2020-08-08 13:29:00 +0200

    decode.scm: add support for I immediate

--- decode.scm
+++ decode.scm
@@ -3,6 +3,12 @@
 (define (instr-opcode instr)
   (bit-field instr 0 6))
 
+(define (instr-funct3 instr)
+  (bit-field instr 12 14))
+
+(define (instr-funct7 instr)
+  (bit-field instr 25 31))
+
 (define (instr-rs1 instr)
   (bit-field instr 15 19))
 
@@ -12,8 +18,5 @@
 (define (instr-rd instr)
   (bit-field instr 7 11))
 
-(define (instr-funct3 instr)
-  (bit-field instr 12 14))
-
-(define (instr-funct7 instr)
-  (bit-field instr 25 31))
+(define (instr-i-imm instr)
+  (bit-field instr 20 31))
