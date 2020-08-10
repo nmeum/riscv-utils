55e246c55bf0ad4fd55c1b140f176e0bcaf1107f
Sören Tempel <tempel@uni-bremen.de>
2020-08-10 17:58:25 +0200

    Improve macro for creating new instructions
    This gets rid of the global variable by using a recursive macro and
    slightly changing the syntax of the new-instr macro.

--- riscv/encode.scm
+++ riscv/encode.scm
@@ -1,35 +1,34 @@
 (import srfi-151)
 
-;; TODO: This would be cleaner without a global variable.
-;; Not proficient enough with macros to achieve this though.
-(define field-pos 32)
-(define-syntax new-instr
-  (syntax-rules ()
-    ((new-instr body ...)
-     (begin
-       (set! field-pos 32)
-       (bitwise-ior body ...)))))
-(define (new-field value size)
+(define (new-field value position size)
   (if (> value (expt 2 (- size 1)))
     (error "given value too large for field")
     (begin
-      (set! field-pos (- field-pos size))
-      (arithmetic-shift value field-pos))))
+      (arithmetic-shift value position))))
+
+(define-syntax new-instr
+  (syntax-rules ()
+    ((new-instr N (var siz))
+     (new-field var (- N siz) siz))
+    ((new-instr N (var siz) fields ...)
+     (bitwise-ior
+       (new-instr N (var siz))
+       (new-instr (- N siz) fields ...)))))
 
 (define (r-type opcode funct3 funct7
           rs1 rs2 rd)
-  (new-instr
-    (new-field funct7 7)
-    (new-field rs2 5)
-    (new-field rs1 5)
-    (new-field funct3 3)
-    (new-field rd 5)
-    (new-field opcode 7)))
+  (new-instr 32
+    (funct7 7)
+    (rs2 5)
+    (rs1 5)
+    (funct3 3)
+    (rd 5)
+    (opcode 7)))
 
 (define (i-type opcode funct3 rs1 rd imm)
-  (new-instr
-    (new-field imm 12)
-    (new-field rs1 5)
-    (new-field funct3 3)
-    (new-field rd 5)
-    (new-field opcode 7)))
+  (new-instr 32
+    (imm 12)
+    (rs1 5)
+    (funct3 3)
+    (rd 5)
+    (opcode 7)))
